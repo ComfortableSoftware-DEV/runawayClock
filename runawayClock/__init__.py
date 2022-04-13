216a34eb0ec0d2d703a68fc958b9f1f746843d80
GaelicGrime <will.angus.blaylock@gmail.com>
2022-04-12 21:57:28 -0600

    Updated __init__.py.

--- runawayClock/__init__.py
+++ runawayClock/__init__.py
@@ -5,17 +5,18 @@ from CF.SUBM_D import _00_OS as CF_OS
 
 
 __all__ = []
+DS_init = {
+    "K_VERSION": 0,
+    "K_DEFAULT": 1,
+    "K_LAST": 0,
+}
+_modulesList_ = CF_OS.globFilePiecesNumsList(CF_OS.DIR_NAME(__file__) + "/*", goodExtList_=[".py"])
+MODULES = {}
 
 
-def __main__():
-
-  __modulesList__ = CF_OS.globFilePiecesNumsList(CF_OS.DIR_NAME(__file__) + "/*", goodExtList_=[".py"])
-  for __thisEntry__ in __modulesList__:
-    __thisFile__ = __thisEntry__[2]
-    if (
-        (__thisFile__ != "__init__.py")
-    ):
-      __all__.append(__thisFile__)
-
-
-__main__()
+for __thisEntry__ in _modulesList_:
+  __thisFile__ = __thisEntry__[2]
+  if (
+      (__thisFile__ != "__init__.py")
+  ):
+    __all__.append(__thisFile__)
